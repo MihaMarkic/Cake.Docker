19eda6fa7b2f71c834124e61efe4063ec0a6f3ba
Miha Markič <miha@rthand.com>
2016-08-29 12:40:01 +0200

    Added Docker Save

---
+++ src/Cake.Docker/Save/Docker.Aliases.Save.cs
@@ -0,0 +1,31 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+
+namespace Cake.Docker
+{
+    partial class DockerAliases
+    {
+        /// <summary>
+        /// Stops an array of <paramref name="images"/> using the give <paramref name="settings"/>.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="settings"></param>
+        /// <param name="images"></param>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Docker")]
+		public static void DockerSave(this ICakeContext context, DockerSaveSettings settings, params string[] images)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            if (images == null || images.Length == 0)
+            {
+                throw new ArgumentNullException("images");
+            }
+            var runner = new GenericDockerRunner<DockerSaveSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Globber);
+            runner.Run("save", settings ?? new DockerSaveSettings(), images);
+        }
+    }
+}
---
+++ src/Cake.Docker/Save/DockerSaveSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    ///  Settings for docker stop
+    /// </summary>
+    public sealed class DockerSaveSettings: AutoToolSettings
+    {
+        /// <summary>
+        ///  Seconds to wait for stop before killing it.
+        /// </summary>
+        public string OutputString { get; set; }
+    }
+}
