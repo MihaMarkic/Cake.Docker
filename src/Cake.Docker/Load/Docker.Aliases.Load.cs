32d38ade3ea786b8ba25d33ccd2b274a32061da5
Miha Markič <miha@rthand.com>
2016-08-29 12:47:05 +0200

    added docker load

---
+++ src/Cake.Docker/Load/Docker.Aliases.Load.cs
@@ -0,0 +1,27 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+
+namespace Cake.Docker
+{
+    partial class DockerAliases
+    {
+        /// <summary>
+        /// Load an image from a tar archive or STDIN.
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="settings"></param>
+        /// <param name="images"></param>
+        [CakeMethodAlias]
+        [CakeAliasCategory("Docker")]
+		public static void DockerLoad(this ICakeContext context, DockerLoadSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            var runner = new GenericDockerRunner<DockerLoadSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Globber);
+            runner.Run("load", settings ?? new DockerLoadSettings(), new string[0]);
+        }
+    }
+}
---
+++ src/Cake.Docker/Load/DockerLoadSettings.cs
@@ -0,0 +1,14 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    ///  Settings for docker stop
+    /// </summary>
+    public sealed class DockerLoadSettings: AutoToolSettings
+    {
+        /// <summary>
+        /// Read from tar archive file, instead of STDIN.
+        /// The tarball may be compressed with gzip, bzip, or xz
+        /// </summary>
+        public string InputString { get; set; }
+    }
+}
