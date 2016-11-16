967c10592d82960585efc53121ca26cbc0d31161
Miha Markič <miha@rthand.com>
2016-11-16 16:38:33 +0100

    added docker-compose create

---
+++ src/Cake.Docker/Compose/Create/Docker.Aliases.ComposeBuild.cs
@@ -0,0 +1,46 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+
+namespace Cake.Docker
+{
+    /// <summary>
+    /// Contains functionality for working with docker-compose build command.
+    /// </summary>
+    [CakeAliasCategory("Docker")]
+    partial class DockerAliases
+    {
+        /// <summary>
+        /// Builds an image using default settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="path">The path.</param>
+        [CakeMethodAlias]
+        public static void DockerComposeCreate(this ICakeContext context, string path)
+        {
+            DockerComposeCreate(context, new DockerComposeCreateSettings(), path);
+        }
+
+        /// <summary>
+        /// Builds an image given <paramref name="settings"/>.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="path">The path.</param>
+        /// <param name="settings">The settings.</param>
+        [CakeMethodAlias]
+        public static void DockerComposeCreate(this ICakeContext context, DockerComposeCreateSettings settings, string path)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            if (string.IsNullOrEmpty(path))
+            {
+                throw new ArgumentNullException("path");
+            }
+            var runner = new GenericDockerRunner<DockerComposeCreateSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Globber);
+            runner.Run("docker-compose create", settings ?? new DockerComposeCreateSettings(), new string[] { path });
+        }
+
+    }
+}
---
+++ src/Cake.Docker/Compose/Create/DockerComposeCreateSettings.cs
@@ -0,0 +1,25 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    /// Settings for docker build.
+    /// </summary>
+    public sealed class DockerComposeCreateSettings: AutoToolSettings
+    {
+        /// <summary>
+        /// Recreate containers even if their configuration and image haven't changed. Incompatible with --no-recreate.
+        /// </summary>
+        public bool ForceRecreate { get; set; }
+        /// <summary>
+        /// If containers already exist, don't recreate them. Incompatible with --force-recreate.
+        /// </summary>
+        public bool NoRecreate { get; set; }
+        /// <summary>
+        /// Don't build an image, even if it's missing.
+        /// </summary>
+        public bool NoBuild { get; set; }
+        /// <summary>
+        /// Build images before creating containers.
+        /// </summary>
+        public bool Build { get; set; }
+    }
+}
