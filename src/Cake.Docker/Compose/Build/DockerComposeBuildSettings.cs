3544e2479342e725375c6848bb559d04195cfdc3
Miha Markič <miha@rthand.com>
2016-11-16 16:35:52 +0100

    added docker-compose build

---
+++ src/Cake.Docker/Compose/Build/DockerComposeBuildSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    /// Settings for docker build.
+    /// </summary>
+    public sealed class DockerComposeBuildSettings: AutoToolSettings
+    {
+        /// <summary>
+        /// Always remove intermediate containers.
+        /// </summary>
+        public bool ForceRm { get; set; }
+        /// <summary>
+        /// Do not use cache when building the image.
+        /// </summary>
+        public bool NoCache { get; set; }
+        /// <summary>
+        /// Always attempt to pull a newer version of the image.
+        /// </summary>
+        public bool Pull { get; set; }
+    }
+}
