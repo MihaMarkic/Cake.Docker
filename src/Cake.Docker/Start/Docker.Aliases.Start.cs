43e0e4cfd321943c2fd5d2c44b2cde16c7691b83
Fabiano Arruda <fabiano.arruda@hotmail.com>
2017-06-10 18:31:31 +0100

    add start command
    Signed-off-by: Fabiano Arruda <fabiano.arruda@hotmail.com>

---
+++ src/Cake.Docker/Start/Docker.Aliases.Start.cs
@@ -0,0 +1,43 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+
+namespace Cake.Docker
+{
+    // Contains functionality for working with start command.
+    partial class DockerAliases
+    {
+        /// <summary>
+        /// Stops an array of <paramref name="containers"/> using default settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="containers">The list of containers.</param>
+        [CakeMethodAlias]
+        public static void DockerStart(this ICakeContext context, params string[] containers)
+        {
+            DockerStart(context, new DockerStartSettings(), containers);
+        }
+        
+        /// <summary>
+        /// Stops an array of <paramref name="containers"/> using the give <paramref name="settings"/>.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="containers">The list of containers.</param>
+        /// <param name="settings">The settings.</param>
+        [CakeMethodAlias]
+		public static void DockerStart(this ICakeContext context, DockerStartSettings settings, params string[] containers)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            if (containers == null || containers.Length == 0)
+            {
+                throw new ArgumentNullException("containers");
+            }
+
+            var runner = new GenericDockerRunner<DockerStartSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+            runner.Run("start", settings ?? new DockerStartSettings(), containers);
+        }
+    }
+}
---
+++ src/Cake.Docker/Start/DockerStartSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    ///  Settings for docker Start
+    /// </summary>
+    public sealed class DockerStartSettings: AutoToolSettings
+    {
+        /// <summary>
+        ///  Seconds to wait for stop before killing it.
+        /// </summary>
+        public int? Time { get; set; }
+    }
+}
