bad6b4b1cac269c37400898484d16254244a8423
Fabiano Arruda <fabiano.arruda@hotmail.com>
2017-06-10 18:29:41 +0100

    add exec command
    Signed-off-by: Fabiano Arruda <fabiano.arruda@hotmail.com>

---
+++ src/Cake.Docker/Exec/DockerExecSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    /// Settings for docker exec command.
+    /// </summary>
+    public sealed class DockerExecSettings: AutoToolSettings
+    {
+        /// <summary>
+        /// Always follow symbol link in SRC_PATH
+        /// </summary>
+        public bool FollowLink { get; set; }
+    }
+}
