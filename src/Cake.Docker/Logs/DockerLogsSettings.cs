befe81f9b6e3872d2ed5b12824f235f216701f9f
Fabiano Arruda <fabiano.arruda@hotmail.com>
2017-06-10 18:30:20 +0100

    add logs command
    Signed-off-by: Fabiano Arruda <fabiano.arruda@hotmail.com>

---
+++ src/Cake.Docker/Logs/DockerLogsSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    ///  Settings for docker Logs
+    /// </summary>
+    public sealed class DockerLogsSettings: AutoToolSettings
+    {
+        /// <summary>
+        ///  Seconds to wait for stop before killing it.
+        /// </summary>
+        public int? Time { get; set; }
+    }
+}
