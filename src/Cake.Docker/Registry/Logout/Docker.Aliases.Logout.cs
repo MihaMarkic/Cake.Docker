1e7c2d3c84b7ab1bf900867475128de4bdab406b
NEXOGEN\kemenya <kemeny.attila@nexogen.hu>
2017-11-16 15:56:05 +0100

    Add logout support

---
+++ src/Cake.Docker/Registry/Logout/Docker.Aliases.Logout.cs
@@ -0,0 +1,23 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+
+namespace Cake.Docker
+{
+    // Contains functionality for working with logout command.
+    partial class DockerAliases
+    {
+        /// <summary>
+        /// Register or log in to a Docker registry.
+        /// If no server is specified, the docker engine default is used.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="server">The server.</param>
+        [CakeMethodAlias]
+        public static void DockerLogout(this ICakeContext context, string server = null)
+        {
+            var runner = new GenericDockerRunner<DockerRegistryLoginSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+            runner.Run("logout", null, server != null ? new[] { server } : null);
+        }
+    }
+}
