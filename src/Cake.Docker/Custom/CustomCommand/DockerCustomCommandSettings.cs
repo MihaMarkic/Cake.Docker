715384355c5992127b1b98bb5217950fb20534ef
NEXOGEN\kemenya <kemeny.attila@nexogen.hu>
2018-01-15 13:05:32 +0100

    Add custom command functionality

---
+++ src/Cake.Docker/Custom/CustomCommand/DockerCustomCommandSettings.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Cake.Docker
+{
+	/// <summary>
+	/// Settings for docker login [OPTIONS] [SERVER].
+	/// Log in to a Docker registry
+	/// </summary>
+	public sealed class DockerCustomCommandSettings : AutoToolSettings
+	{
+	}
+}
