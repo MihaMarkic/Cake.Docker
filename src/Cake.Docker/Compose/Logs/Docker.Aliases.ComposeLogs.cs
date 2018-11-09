81d704f575b5ffd02470db0da26f2ed4b51f68a2
Miha Markič <miha@rthand.com>
2018-11-09 19:20:30 +0100

    Closes #51 Implemented docker-compose logs

---
+++ src/Cake.Docker/Compose/Logs/Docker.Aliases.ComposeLogs.cs
@@ -0,0 +1,37 @@
+﻿using Cake.Core;
+using Cake.Core.Annotations;
+using System;
+
+namespace Cake.Docker
+{
+    // Contains functionality for working with docker-compose down command.
+    partial class DockerAliases
+    {
+        /// <summary>
+        /// Runs docker-compose kill with default settings.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        [CakeMethodAlias]
+        public static void DockerComposeLogs(this ICakeContext context)
+        {
+            DockerComposeLogs(context, new DockerComposeLogsSettings());
+        }
+
+        /// <summary>
+        /// Runs docker-compose kill given <paramref name="settings"/>.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">The settings.</param>
+        [CakeMethodAlias]
+        public static void DockerComposeLogs(this ICakeContext context, DockerComposeLogsSettings settings)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            var runner = new GenericDockerComposeRunner<DockerComposeLogsSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+            runner.Run("logs", settings ?? new DockerComposeLogsSettings(), new string[0]);
+        }
+
+    }
+}
---
+++ src/Cake.Docker/Compose/Logs/DockerComposeLogsSettings.cs
@@ -0,0 +1,30 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    /// Settings for docker build.
+    /// </summary>
+    public sealed class DockerComposeLogsSettings: DockerComposeSettings
+    {
+        /// <summary>
+        /// Produce monochrome output.
+        /// </summary>
+        public bool NoColor { get; set; }
+        /// <summary>
+		/// --follow, -f
+		/// default: false
+		/// Follow log output
+		/// </summary>
+		public bool? Follow { get; set; }
+        /// <summary>
+		/// -t, --timestamps
+		/// default: false
+		/// Show timestamps
+		/// </summary>
+		public bool? Timestamps { get; set; }
+        /// <summary>
+        /// --tail="all"
+        /// Number of lines to show from the end of the logs for each container.
+        /// </summary>
+        public string Tail { get; set; }
+    }
+}
