de912be874e96792fff608fcefac5bab9ca625ac
Miha Markič <miha@rthand.com>
2018-11-09 19:09:26 +0100

    Closes #50 Creates a DockerRunWithoutResult to allow capturing of stdout.

--- src/Cake.Docker/Container/Run/Docker.Aliases.Run.cs
+++ src/Cake.Docker/Container/Run/Docker.Aliases.Run.cs
@@ -17,6 +17,8 @@ namespace Cake.Docker
         /// <param name="image">The image.</param>
         /// <param name="args">The arguments.</param>
         /// <param name="command">The command.</param>
+        /// <returns>Container ID</returns>
+        /// <remarks>Return value is first line from stdout. This method will redirect stdout and it won't be available for capture.</remarks>
         [CakeMethodAlias]
         public static string DockerRun(this ICakeContext context, string image, string command, params string[] args)
         {
@@ -31,6 +33,8 @@ namespace Cake.Docker
         /// <param name="image">The image.</param>
         /// <param name="args">The arguments.</param>
         /// <param name="command">The command.</param>
+        /// <returns>Container ID</returns>
+        /// <remarks>Return value is first line from stdout. This method will redirect stdout and it won't be available for capture.</remarks>
         [CakeMethodAlias]
         public static string DockerRun(this ICakeContext context, DockerContainerRunSettings settings, string image, string command, params string[] args)
         {
@@ -54,6 +58,52 @@ namespace Cake.Docker
             }
             return runner.RunWithResult("run", settings ?? new DockerContainerRunSettings(), r => r.ToArray(), arguments.ToArray()).FirstOrDefault();
         }
-
+        /// <summary>
+        /// Creates a new container using default settings. and doesn't return the output like 
+        /// <see cref="DockerRun(ICakeContext, string, string, string[])"/> does.
+        /// This way one can capture stdout.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="image">The image.</param>
+        /// <param name="args">The arguments.</param>
+        /// <param name="command">The command.</param>
+        [CakeMethodAlias]
+        public static void DockerRunWithoutResult(this ICakeContext context, string image, string command, params string[] args)
+        {
+            DockerRunWithoutResult(context, new DockerContainerRunSettings(), image, command, args);
+        }
+        /// <summary>
+        /// Creates a new container given <paramref name="settings"/> and doesn't return the output like 
+        /// <see cref="DockerRun(ICakeContext, DockerContainerRunSettings, string, string, string[])"/> does.
+        /// This way one can capture stdout.
+        /// </summary>
+        /// <param name="context">The context.</param>
+        /// <param name="settings">The settings.</param>
+        /// <param name="image">The image.</param>
+        /// <param name="args">The arguments.</param>
+        /// <param name="command">The command.</param>
+        [CakeMethodAlias]
+        public static void DockerRunWithoutResult(this ICakeContext context, DockerContainerRunSettings settings, string image, string command, params string[] args)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException("context");
+            }
+            if (string.IsNullOrEmpty(image))
+            {
+                throw new ArgumentNullException("image");
+            }
+            var runner = new GenericDockerRunner<DockerContainerRunSettings>(context.FileSystem, context.Environment, context.ProcessRunner, context.Tools);
+            List<string> arguments = new List<string> { image };
+            if (!string.IsNullOrEmpty(command))
+            {
+                arguments.Add(command);
+                if (args.Length > 0)
+                {
+                    arguments.AddRange(args);
+                }
+            }
+            runner.Run("run", settings ?? new DockerContainerRunSettings(), arguments.ToArray());
+        }
     }
 }
