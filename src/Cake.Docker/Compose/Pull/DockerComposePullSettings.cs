5ca6003b191231635f8e512ceeca85deeaba2d04
Miha Markič <miha@rthand.com>
2019-02-14 10:01:58 +0100

    Fixes #56 - adds bunch of settings to docker-compose pull

--- src/Cake.Docker/Compose/Pull/DockerComposePullSettings.cs
+++ src/Cake.Docker/Compose/Pull/DockerComposePullSettings.cs
@@ -9,7 +9,17 @@
         /// Pull what it can and ignores images with pull failures.
         /// </summary>
         public bool IgnorePullFailures { get; set; }
-
-
+        /// <summary>
+        /// Disable parallel pulling.
+        /// </summary>
+        public bool NoParallel { get; set; }
+        /// <summary>
+        /// Also pull services declared as dependencies
+        /// </summary>
+        public bool IncludeDeps { get; set; }
+        /// <summary>
+        /// Pull without printing progress information
+        /// </summary>
+        public bool Quiet { get; set; }
     }
 }
