7ade56ae1013095db552075b7ff338fd3ac48764
Elias Oehen <elias.oehen@gmail.com>
2020-12-29 14:13:52 +0100

    Add docker-compose push support

---
+++ src/Cake.Docker/Compose/Push/DockerComposePublishSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    /// Settings for docker publish.
+    /// </summary>
+    public sealed class DockerComposePublishSettings: DockerComposeSettings
+    {
+    }
+}
