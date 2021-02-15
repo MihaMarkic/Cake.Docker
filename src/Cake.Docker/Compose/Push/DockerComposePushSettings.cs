74ae0d42b2da0ffef02fc3fab72f1f7ba9a5f1b5
Miha Markič <miha@rthand.com>
2021-02-15 09:24:32 +0100

    Adds mising DockerComposePush files

---
+++ src/Cake.Docker/Compose/Push/DockerComposePushSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    /// Settings for docker publish.
+    /// </summary>
+    public sealed class DockerComposePublishSettings: DockerComposeSettings
+    {
+        /// <summary>
+        /// Push what it can and ignores images with push failures.
+        /// </summary>
+        public bool IgnorePushFailures { get; set; }
+    }
+}
