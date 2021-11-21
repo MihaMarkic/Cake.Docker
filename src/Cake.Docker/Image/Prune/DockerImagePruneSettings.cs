c02a3e45f4539db4e72dd0e6b741948bc60c8368
Miha Markič <miha@rthand.com>
2021-11-21 12:08:28 +0100

    Updates settings generator and Image Prune settings

--- src/Cake.Docker/Image/Prune/DockerImagePruneSettings.cs
+++ src/Cake.Docker/Image/Prune/DockerImagePruneSettings.cs
@@ -1,21 +1,27 @@
-﻿namespace Cake.Docker
+namespace Cake.Docker
 {
     /// <summary>
-    /// Settings for docker volume prune.
+    /// Settings for docker prune [OPTIONS].
+    /// Remove unused images
     /// </summary>
-    public sealed class DockerImagePruneSettings: AutoToolSettings
-    {
-        /// <summary>
-        /// Remove all unused images, not just dangling ones
-        /// </summary>
-        public bool All { get; set; }
-        /// <summary>
-        /// Provide filter values (e.g. 'until=&lt;timestamp&gt;')
-        /// </summary>
-        public string Filter { get; set; }
-        /// <summary>
-        /// Do not prompt for confirmation
-        /// </summary>
-        public bool Force { get; set; }
-    }
+    public sealed partial class DockerImagePruneSettings : AutoToolSettings
+	{
+		/// <summary>
+		/// --all, -a
+		/// default: false
+		/// Remove all unused images, not just dangling ones
+		/// </summary>
+		public bool? All { get; set; }
+		/// <summary>
+		/// --filter
+		/// Provide filter values (e.g. &#39;until=&lt;timestamp&gt;&#39;)
+		/// </summary>
+		public string Filter { get; set; }
+		/// <summary>
+		/// --force, -f
+		/// default: false
+		/// Do not prompt for confirmation
+		/// </summary>
+		public bool? Force { get; set; }
+	}
 }
--- src/SettingsGenerator/SettingsGenerator/Program.cs
+++ src/SettingsGenerator/SettingsGenerator/Program.cs
@@ -1,41 +1,15 @@
 ﻿using System;
 using System.IO;
-using System.Threading.Tasks;
 
 namespace SettingsGenerator
 {
-    class Program
+    partial class Program
     {
         const string root = @"D:\GitProjects\Righthand\Cake\Cake.Docker\src\Cake.Docker\";
-        static Input[] inputs = new Input[] {
-    //new Input(Path.Combine("Image", "Build")),
-    //new Input(Path.Combine("Image", "Load")),
-    //new Input(Path.Combine("Image", "Pull")),
-    //new Input(Path.Combine("Image", "Push")),
-    //new Input(Path.Combine("Image", "Remove")),
-    //new Input(Path.Combine("Image", "Save")),
-    //new Input(Path.Combine("Image", "Tag")),
-    //new Input(Path.Combine("Container", "Cp"), "Copy"),
-    //new Input(Path.Combine("Container", "Create"), inputTypeOptions: InputTypeOptions.Container, options: Option.NoDuplicateCreate),
-    //new Input(Path.Combine("Container", "Exec")),
-    //new Input(Path.Combine("Container", "Logs")),
-    //new Input(Path.Combine("Container", "Rm")),
-    //new Input(Path.Combine("Container", "Run"), inputTypeOptions: InputTypeOptions.Container),
-    //new Input(Path.Combine("Container", "Start")),
-    //new Input(Path.Combine("Container", "Stop")),
-    //new Input(Path.Combine("Registry", "Login")),
-    //new Input(Path.Combine("Swarm", "Init"), inputTypeOptions: InputTypeOptions.Swarm),
-    //new Input(Path.Combine("Swarm", "Join"), inputTypeOptions: InputTypeOptions.SwarmConsts),
-    //new Input(Path.Combine("Swarm", "Leave")),
-    //new Input(Path.Combine("Swarm", "Update"), inputTypeOptions: InputTypeOptions.Swarm),
-    //new Input(Path.Combine("Network", "Connect")),
-    //new Input(Path.Combine("Network", "Create"), options: Option.TypeContainsBlankLines),
-    //new Input(Path.Combine("Network", "Disconnect")),
-    new Input(Path.Combine("Network", "Remove"))
-};
 
         public static void Main(string[] args)
         {
+            string root = Path.Combine(Path.GetDirectoryName(typeof(Program).Assembly.Location), @"../../../../../Cake.Docker");
             var processor = new Processor(root);
             foreach (var input in inputs)
             {
