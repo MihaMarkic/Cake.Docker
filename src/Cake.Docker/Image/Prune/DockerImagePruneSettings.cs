7a5c47322007581db4b2c491d7690b1230731ad8
Miha Markič <miha@rthand.com>
2021-11-21 11:36:24 +0100

    Closes #89 Adds image prune

---
+++ src/Cake.Docker/Image/Prune/DockerImagePruneSettings.cs
@@ -0,0 +1,21 @@
+﻿namespace Cake.Docker
+{
+    /// <summary>
+    /// Settings for docker volume prune.
+    /// </summary>
+    public sealed class DockerImagePruneSettings: AutoToolSettings
+    {
+        /// <summary>
+        /// Remove all unused images, not just dangling ones
+        /// </summary>
+        public bool All { get; set; }
+        /// <summary>
+        /// Provide filter values (e.g. 'until=&lt;timestamp&gt;')
+        /// </summary>
+        public string Filter { get; set; }
+        /// <summary>
+        /// Do not prompt for confirmation
+        /// </summary>
+        public bool Force { get; set; }
+    }
+}
